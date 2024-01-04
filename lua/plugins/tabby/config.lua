e841a4d70b6b05d45d6914222274e3f3cf0b8db5
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:51:38 -0800

    iterate styles

--- lua/plugins/tabby/config.lua
+++ lua/plugins/tabby/config.lua
@@ -3,9 +3,8 @@ local theme = {
   fill = 'TabLineFill',
   -- Also you can do this: fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
   head = 'TabLineSel',
-  current_tab = 'TabLineSel',
-  tab = 'TabLine',
-  win = 'TabLine',
+  current_tab = 'TabLine',
+  tab = 'TabLineFill',
   tail = 'TabLineSel',
 }
 require('tabby.tabline').set(function(line)
@@ -15,24 +14,26 @@ require('tabby.tabline').set(function(line)
       line.sep('', theme.head, theme.fill),
     },
     line.tabs().foreach(function(tab)
+      local hl = tab.is_current() and theme.current_tab or theme.fill
       return {
-        line.sep('', theme.tab, theme.fill),
+        line.sep(tab.is_current() and '' or '', hl, theme.fill),
         tab.is_current() and '' or '',
         tab.number(),
         tab.name(),
-        line.sep('', theme.tab, theme.fill),
-        hl = theme.tab,
+        line.sep(tab.is_current() and '' or '', hl, theme.fill),
+        hl = hl,
         margin = ' ',
       }
     end),
     line.spacer(),
     line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
+      local hl = win.is_current() and theme.current_tab or theme.fill
       return {
-        line.sep('', theme.win, theme.fill),
+        line.sep(win.is_current() and '' or '', hl, theme.fill),
         win.is_current() and '' or '',
         win.buf_name(),
-        line.sep('', theme.win, theme.fill),
-        hl = theme.win,
+        line.sep(win.is_current() and '' or '', hl, theme.fill),
+        hl = hl,
         margin = ' ',
       }
     end),
@@ -49,14 +50,8 @@ end, {
     end
   }
 })
-vim.api.nvim_create_user_command(
-  'TR',
-  "TabRename <args>",
-  { nargs = "?", bang = true }
-)
 
-vim.api.nvim_create_user_command(
-  'TS',
-  "tab split",
-  { bang = true }
-)
+vim.api.nvim_create_user_command('TR', "TabRename <args>", { nargs = "?", bang = true })
+vim.api.nvim_create_user_command('TS', "tab split", { bang = true })
+vim.api.nvim_create_user_command('VS', "vs", { bang = true })
+vim.api.nvim_create_user_command('SP', "sp", { bang = true })
