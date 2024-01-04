4a34eb77e974b6464e5f74580f7239bb6ac87226
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:51:36 -0800

    fix lsp command

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -12,10 +12,10 @@ require("mason-lspconfig").setup({
   automatic_installation = { exclude = { "rust_analyzer", "omnisharp", "omnisharp_mono" } }
 })
 
-local opts = { noremap=true, silent=true }
+local opts = { noremap=true }
 vim.api.nvim_set_keymap('n', '<leader>nt', ":LspRestart<CR>", opts)
 vim.api.nvim_set_keymap('n', '<leader>ns', ":LspStop<CR>", opts)
-vim.api.nvim_set_keymap('n', '<leader>nS', ":Lsp<CR>", opts)
+vim.api.nvim_set_keymap('n', '<leader>nS', ":LspStart<CR>", opts)
 
 require('which-key').register({
   n = {
