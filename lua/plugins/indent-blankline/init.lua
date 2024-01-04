29f3b2777422e9595956e07f11e6912a88461da0
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:51:40 -0800

    update plugin versions

--- lua/plugins/indent-blankline/init.lua
+++ lua/plugins/indent-blankline/init.lua
@@ -1,5 +1,6 @@
 return {
   'lukas-reineke/indent-blankline.nvim',
+  version = '2.20.8', -- @TODO: update this
   event = 'VeryLazy',
   config = function ()
     require('plugins.indent-blankline.config')
