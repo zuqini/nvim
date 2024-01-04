a65f821437cc5c0acb1e19d3fd7ebf64f685f3fa
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:51:33 -0800

    Update my.lua

--- lua/plugins/lsp/my.lua
+++ lua/plugins/lsp/my.lua
@@ -1,10 +1,13 @@
 -- put machine specific lsp configs here
+-- NOTE: run the following command to avoid tracking changes to this file
+-- git update-index --assume-unchanged lua/plugins/lsp/my.lua
 
 -- eg.
 -- local lspconfig = require('lspconfig')
 
 -- lspconfig.omnisharp.setup {}
 -- OR
+-- omnisharp_mono comes from mason so need to setup defaults manually
 -- lspconfig.omnisharp_mono.setup {
 --     on_attach = lspconfig.zuqini_custom.on_attach,
 --     capabilities = lspconfig.zuqini_custom.capabilities,
