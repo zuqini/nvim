755f8b5574baa36801ba36e55bb2380ec98f7c2e
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:51:42 -0800

    comment jsx/tsx files

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,8 +1,15 @@
 return {
     'numToStr/Comment.nvim',
+    dependencies = {
+      'JoosepAlviste/nvim-ts-context-commentstring',
+    },
     keys = {
       { 'gc', mode = { 'x', 'v', 'n' } },
       { 'gb', mode = { 'x', 'v', 'n' } },
     },
-    config = true,
+    config = function ()
+      require('Comment').setup({
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      })
+    end,
 }
