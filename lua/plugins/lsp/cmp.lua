8f561cb95ff30862bdc7cca033d59d725ece9d7b
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:51:37 -0800

    update cmp mapping

--- lua/plugins/lsp/cmp.lua
+++ lua/plugins/lsp/cmp.lua
@@ -20,6 +20,12 @@ vim.keymap.set({ "i", "s" }, "<c-h>", function()
 end)
 
 -- nvim-cmp setup
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local cmp = require 'cmp'
 cmp.setup {
   window = {
@@ -44,16 +50,19 @@ cmp.setup {
     ['<CR>'] = cmp.mapping.confirm {
       select = true,
     },
-    ['<Tab>'] = function(fallback)
+    ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback()
       end
-    end,
-    ['<S-Tab>'] = function(fallback)
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
@@ -61,7 +70,7 @@ cmp.setup {
       else
         fallback()
       end
-    end,
+    end, { "i", "s" }),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
