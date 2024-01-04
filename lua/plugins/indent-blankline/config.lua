f10e147581968a1786834e4ca247f913d2630c16
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:51:38 -0800

    no trailing blankline indent

--- lua/plugins/indent-blankline/config.lua
+++ lua/plugins/indent-blankline/config.lua
@@ -9,6 +9,7 @@ require('indent_blankline').setup {
   show_current_context = true,
   show_current_context_start = true,
   char_highlight_list = hl_list,
+  show_trailing_blankline_indent = false,
 }
 vim.g.indent_blankline_char = '▏'
 vim.g.indent_blankline_context_char = '▎'
