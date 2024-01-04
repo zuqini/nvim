9b0e5402fa97f11438d8a6ea3e9e725bf35fc8de
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:51:40 -0800

    tweak flash autohide for operator pending mode

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,7 +1,6 @@
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
-  opts = {},
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "<c-s>", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
@@ -9,4 +8,21 @@ return {
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
+  opts = {
+    modes = {
+      char = {
+        config = function(opts)
+          -- autohide flash when in operator-pending mode
+          local pending_operators_to_autohide = "yd"
+          opts.autohide = vim.fn.mode(true):find("no") and string.find(pending_operators_to_autohide, vim.v.operator)
+
+          -- disable jump labels when enabled and when using a count
+          opts.jump_labels = opts.jump_labels and vim.v.count == 0
+
+          -- Show jump labels only in operator-pending mode
+          -- opts.jump_labels = vim.v.count == 0 and vim.fn.mode(true):find("o")
+        end
+      }
+    }
+  }
 }
