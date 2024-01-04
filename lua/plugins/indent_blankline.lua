df3e93c6e69b32671f7ca661b5d76ea8321722cc
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:51:35 -0800

    indent_blankline colors

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -1,1 +1,9 @@
-require("indent_blankline").setup {}
+local hl_list = {}
+for i, color in pairs({ '#662121', '#767621', '#216631', '#325a5e', '#324b7b', '#562155' }) do
+  local name = 'IndentBlanklineIndent' .. i
+  vim.api.nvim_set_hl(0, name, { fg = color })
+  table.insert(hl_list, name);
+end
+require('indent_blankline').setup {
+  char_highlight_list = hl_list,
+}
