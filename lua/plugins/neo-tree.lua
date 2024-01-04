a283791817ff9596a3f027e5fd3ec6c38a7defaf
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:51:38 -0800

    iterate tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,7 +2,7 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v2.x",
   keys = {
-    "<Tab>",
+    "<S-Tab>",
     "<Leader><Tab>",
   },
   dependencies = {
@@ -12,7 +12,7 @@ return {
   },
   config = function ()
     local opts = { noremap=true, silent=true }
-    vim.api.nvim_set_keymap('n', '<tab>', ':NeoTreeFocusToggle<CR>', opts)
+    vim.api.nvim_set_keymap('n', '<S-tab>', ':NeoTreeFocusToggle<CR>', opts)
     vim.api.nvim_set_keymap('n', '<leader><tab>', ':NeoTreeRevealToggle<CR>', opts)
     require("neo-tree").setup({
       close_if_last_window = true,
