890ee3e1c30672ba39b3e9dec06600f89bbed911
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:51:41 -0800

    tweak toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,6 +4,7 @@ return {
   config = function()
     require("toggleterm").setup({
       open_mapping = [[<c-\>]],
+      shade_terminals = false,
       size = function (_term)
         return vim.o.lines * 0.6
       end,
