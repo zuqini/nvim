1f1dde1747ca356222756598beeee0af6a88ce1a
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:51:39 -0800

    bump default term size

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,9 +1,12 @@
 return {
   'akinsho/toggleterm.nvim',
   keys = '<c-\\>',
-  config = function ()
+  config = function()
     require("toggleterm").setup({
       open_mapping = [[<c-\>]],
+      size = function (term)
+        return vim.o.lines * 0.75
+      end,
     })
   end
 }
