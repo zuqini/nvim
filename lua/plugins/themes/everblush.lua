4fb45d0529a248982e6b2860d742412b83d964b2
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:51:38 -0800

    lighten certain everblush colors

--- lua/plugins/themes/everblush.lua
+++ lua/plugins/themes/everblush.lua
@@ -1,2 +1,13 @@
 local everblush = require('everblush')
-everblush.setup({ nvim_tree = { contrast = true } }) -- or use contrast = false to not apply contrast
+everblush.setup({
+  override = {
+    Comment = { fg = '#737A7C', italic = true },
+    NeogitHunkHeaderHighlight = { fg = '#737A7C', italic = true },
+    ['@comment'] = { fg = '#737A7C', italic = true },
+
+    ['@float'] = { fg = '#6E7577' },
+    ['@exception'] = { fg = '#6E7577' },
+    ['@text.note'] = { fg = '#6E7577' },
+    ['@text.danger'] = { fg = '#6E7577' },
+  },
+}) -- or use contrast = false to not apply contrast
