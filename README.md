ca634e5232ca686bb5e2142e6103e6066cdcc250
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:51:30 -0800

    tweak lualine

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # nvim
 My Neovim setup
-<img width="1792" alt="Screen Shot 2022-01-16 at 2 22 42 PM" src="https://user-images.githubusercontent.com/5790854/149680520-77117bf3-ecfc-4586-bb6b-42cb1d1009a4.png">
+<img width="1792" alt="Screen Shot 2022-01-17 at 2 31 55 AM" src="https://user-images.githubusercontent.com/5790854/149753476-5594abd9-8185-4c7e-8a19-611bd1a71ccf.png">
 
 ## Install
 <details>
--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -31,7 +31,7 @@ require('lualine').setup {
             {
                 'filename',
                 file_status = true,     -- Displays file status (readonly status, modified status)
-                path = 1,               -- 0: Just the filename
+                path = 0,               -- 0: Just the filename
                                         -- 1: Relative path
                                         -- 2: Absolute path
 
@@ -76,7 +76,24 @@ require('lualine').setup {
             }
         },
         lualine_b = {
-            'branch',
+            {
+                'branch',
+            },
+            {
+                'filename',
+                file_status = true,     -- Displays file status (readonly status, modified status)
+                path = 1,               -- 0: Just the filename
+                                        -- 1: Relative path
+                                        -- 2: Absolute path
+
+                shorting_target = 40,   -- Shortens path to leave 40 spaces in the window
+                                        -- for other components. (terrible name, any suggestions?)
+                symbols = {
+                    modified = '[+]',
+                    readonly = '[-]',
+                    unnamed = '[]',
+                },
+            },
         },
         lualine_c = {
             'diff',
