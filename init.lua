e0c5c7a39c025a585188007e459bb50cd94ca1bd
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:51:36 -0800

    iterate laziness

--- init.lua
+++ init.lua
@@ -12,25 +12,39 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.runtimepath:prepend(lazypath)
 
+function vRequire(path)
+  vim.cmd('source ' .. vim.fn.stdpath('config') .. '/vim/' .. path .. '.vim')
+end
+
 vim.g.mapleader = " " -- make sure to set `mapleader` before lazy so your mappings are correct
 
 require("lazy").setup({
   -- Themes
-  'zuqini/gruvbox-material',
-  'EdenEast/nightfox.nvim',
-  'rebelot/kanagawa.nvim',
+  { 'zuqini/gruvbox-material', lazy = true },
+  { 'EdenEast/nightfox.nvim', lazy = true },
+  { 'rebelot/kanagawa.nvim', lazy = true },
+  { 'folke/tokyonight.nvim', branch = 'main', lazy = true, },
+  { 'pineapplegiant/spaceduck', branch= 'main', lazy = true, },
   {
-    'folke/tokyonight.nvim',
-    branch = 'main'
+    'nvim-lualine/lualine.nvim',
+    lazy = true,
+    dependencies = {
+      'kyazdani42/nvim-web-devicons'
+    },
   },
   {
-    'pineapplegiant/spaceduck',
-    branch= 'main'
+    'kyazdani42/nvim-tree.lua', -- https://github.com/kyazdani42/nvim-tree.lua/issues/877,
+    dependencies = {
+      'kyazdani42/nvim-web-devicons'
+    },
+    config = function()
+      require("plugins/nvim-tree")
+    end,
+    keys = {
+      "<S-Tab>",
+      "<Leader><Tab>",
+    },
   },
-
-  'nvim-lualine/lualine.nvim',
-  'kyazdani42/nvim-web-devicons', -- for file icons
-  'kyazdani42/nvim-tree.lua', -- https://github.com/kyazdani42/nvim-tree.lua/issues/877,
 
   -- Telescope
   -- 'nvim-lua/plenary.nvim',
@@ -53,13 +67,22 @@ require("lazy").setup({
   'williamboman/mason.nvim',
   'williamboman/mason-lspconfig.nvim',
 
-  'hrsh7th/nvim-cmp', -- Autocompletion plugin
-  'hrsh7th/cmp-buffer',
-  'hrsh7th/cmp-path',
-  'hrsh7th/cmp-cmdline',
-  'hrsh7th/cmp-nvim-lsp', -- LSP source for nvim-cmp
-  'saadparwaiz1/cmp_luasnip', -- Snippets source for nvim-cmp
-  'L3MON4D3/LuaSnip', -- Snippets plugin
+  {
+    'hrsh7th/nvim-cmp', -- Autocompletion plugin
+    -- not InsertEnter because I need it for commandline cmp too
+    event = { "InsertEnter", "CmdlineEnter" },
+    dependencies = {
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-nvim-lsp', -- LSP source for nvim-cmp
+      'saadparwaiz1/cmp_luasnip', -- Snippets source for nvim-cmp
+      'L3MON4D3/LuaSnip', -- Snippets plugin
+    },
+    config = function()
+      require("plugins/lsp/cmp")
+    end,
+  },
 
   -- 'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
   'stevearc/vim-arduino',
@@ -69,12 +92,14 @@ require("lazy").setup({
   'nvim-treesitter/nvim-treesitter-textobjects',
   'nvim-treesitter/nvim-treesitter-context',
   'drybalka/tree-climber.nvim',
+
   -- Additional language packs that treesitter doesn't support
   'elubow/cql-vim', -- CQL syntax highlight for cassandra
 
   --  Utils
   'nvim-lua/plenary.nvim',
   'ThePrimeagen/harpoon', -- needs plenary
+  'ggandor/leap.nvim', -- breaking changes: https://github.com/ggandor/leap.nvim/issues/18
   'darfink/vim-plist',
   'petertriho/nvim-scrollbar',
   'dstein64/vim-startuptime',
@@ -88,20 +113,18 @@ require("lazy").setup({
 
   'michaeljsmith/vim-indent-object', -- ii, ai, iI, aI
   'lukas-reineke/indent-blankline.nvim',
-  'ggandor/leap.nvim', -- breaking changes: https://github.com/ggandor/leap.nvim/issues/18
+}, {
+  defaults = {
+  },
 })
 
-function requireVim(path)
-  vim.cmd('source ' .. vim.fn.stdpath('config') .. '/vim/' .. path .. '.vim')
-end
 
 -- require helpers.lua first - it defines helper functions that other config files may use
 require("helpers")
 
-requireVim("main")
-requireVim("neovide")
+vRequire("main")
+vRequire("neovide")
 
-require("plugins/nvim-tree")
 
 require("plugins/treesitter/treesitter")
 require("plugins/treesitter/context")
@@ -109,23 +132,21 @@ require("plugins/treesitter/treeclimber")
 
 require("plugins/lsp/mason")
 require("plugins/lsp/lsp")
-require("plugins/lsp/cmp")
 pcall(function() require("plugins/lsp/my") end) -- this is for env specific lsp configs
 
-require("plugins/lualine")
 require("plugins/gitsigns")
 require("plugins/indent_blankline")
 require("plugins/leap")
 require("plugins/whichkey")
 require("plugins/harpoon")
 require("plugins/misc")
-requireVim("plugins/undotree")
-requireVim("plugins/closetag")
+vRequire("plugins/undotree")
+vRequire("plugins/closetag")
 -- call RequireVim("plugins/nutoggle")
-requireVim("plugins/plist")
+vRequire("plugins/plist")
 
 if vim.g.is_windows then
-  requireVim("plugins/fzf")
+  vRequire("plugins/fzf")
 else
   require("plugins/fzf-lua")
 end
