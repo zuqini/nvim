0cfcbd67f37533777c4e3c12d04ac53b908a73aa
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:51:32 -0800

    cursor color

--- lua/themes/gruvbox.lua
+++ lua/themes/gruvbox.lua
@@ -1,3 +1,4 @@
 vim.g.gruvbox_invert_selection=0
 vim.g.gruvbox_italic=1
 vim.cmd[[colorscheme gruvbox]]
+vim.api.nvim_set_hl(0, 'Cursor', { fg='#504945', bg='#ebdbb2' })
--- lua/themes/kanagawa.lua
+++ lua/themes/kanagawa.lua
@@ -1,1 +1,2 @@
 vim.cmd[[colorscheme kanagawa]]
+vim.api.nvim_set_hl(0, 'Cursor', { fg='#1a1b26', bg='#c0caf5' })
