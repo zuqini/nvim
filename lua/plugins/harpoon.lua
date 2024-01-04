e91d707f3ecab3e9ea6b771c9c7be249010dde0a
zuqini <lizuqiliang@gmail.com>
2024-01-04 12:51:34 -0800

    update harpoon mapping again

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,6 +1,6 @@
 local opts = { noremap=true, silent=false}
 vim.keymap.set('n', ',p', require("harpoon.mark").add_file, opts)
-vim.keymap.set('n', ',m', require("harpoon.ui").toggle_quick_menu, opts)
+vim.keymap.set('n', ',h', require("harpoon.ui").toggle_quick_menu, opts)
 
 vim.keymap.set('n', ',a', function() require("harpoon.ui").nav_file(1) end, opts)
 vim.keymap.set('n', ',r', function() require("harpoon.ui").nav_file(2) end, opts)
