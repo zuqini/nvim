ba57a5e3e8c61fa1d91fe66d20c691de009e4519
zuqini <lizuqiliang@gmail.com>
2024-06-28 19:57:39 -0700

    speedtyper

---
+++ lua/plugins/speedtyper.lua
@@ -0,0 +1,10 @@
+return {
+    "NStefan002/speedtyper.nvim",
+    cmd = "Speedtyper",
+    opts = {
+      window = {
+        close_with = { n = "q", i = "<A-q>" },
+      },
+      sentence_mode = true,
+    },
+}
