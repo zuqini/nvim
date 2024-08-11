e6929aee409e4395a0c76bab0e0280ef4651a1a4
zuqini <lizuqiliang@gmail.com>
2024-08-11 11:44:28 -0700

    update deprecated sign_define api

--- lua/plugins/lualine/config.lua
+++ lua/plugins/lualine/config.lua
@@ -13,12 +13,29 @@ local angle_up_thin2_seps = { left = '', right = '' }
 local triangle_thin_seps = { left = '', right = '' }
 local circle_thin_seps = { left = '', right = '' }
 
-local signs = { Error = "󰅚", Warn = "󰀪", Hint = "󰌶", Info = "󰋽" }
 local symbols = { error = "󰅚  ", warn = "󰀪  ", hint = "󰌶  ", info = "󰋽  " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '󰅚',
+      [vim.diagnostic.severity.WARN] = '󰀪',
+      [vim.diagnostic.severity.HINT] = '󰌶',
+      [vim.diagnostic.severity.INFO] = '󰋽',
+    },
+    linehl = {
+      [vim.diagnostic.severity.ERROR] = 'DiagnosticSignError',
+      [vim.diagnostic.severity.WARN] = 'DiagnosticSignWarn',
+      [vim.diagnostic.severity.HINT] = 'DiagnosticSignHint',
+      [vim.diagnostic.severity.INFO] = 'DiagnosticSignInfo',
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = 'DiagnosticSignError',
+      [vim.diagnostic.severity.WARN] = 'DiagnosticSignWarn',
+      [vim.diagnostic.severity.HINT] = 'DiagnosticSignHint',
+      [vim.diagnostic.severity.INFO] = 'DiagnosticSignInfo',
+    },
+  }
+})
 
 local navic = require("nvim-navic")
 local winbar_navic = {
