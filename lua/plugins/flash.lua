0a4ed7adb5cc918ea7de341f604c6b126db2b62e
zuqini <lizuqiliang@gmail.com>
2024-11-30 16:08:44 -0800

    nvim-surround -> mini.surround

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,7 +2,7 @@ return {
   "folke/flash.nvim",
   event = "VeryLazy",
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "<leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "<c-s>", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -15,8 +15,15 @@ return {
       vrequire('plugins/undotree')
     end
   },
-  { 'kylechui/nvim-surround', event = 'VeryLazy', config = true }, -- @TODO: lazy load with keys
-  { 'stevearc/vim-arduino',   ft = "arduino", },
-  { 'habamax/vim-godot',      ft = "gdscript" },
-  { 'tpope/vim-fugitive',     cmd = 'G' },
+  -- { 'kylechui/nvim-surround', event = 'VeryLazy', config = true }, -- @TODO: lazy load with keys
+  {
+    'echasnovski/mini.surround',
+    version = false,
+    config = function()
+      require('mini.surround').setup()
+    end
+  },
+  { 'stevearc/vim-arduino', ft = "arduino", },
+  { 'habamax/vim-godot',    ft = "gdscript" },
+  { 'tpope/vim-fugitive',   cmd = 'G' },
 }
