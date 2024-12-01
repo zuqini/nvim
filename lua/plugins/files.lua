cbec2961f7328000cd64361d68a1ea63b8433057
zuqini <lizuqiliang@gmail.com>
2024-12-01 15:09:44 -0800

    files preview

--- lua/plugins/files.lua
+++ lua/plugins/files.lua
@@ -10,6 +10,9 @@ return {
           go_out      = 'H',
           go_out_plus = '<ESC>',
         },
+        windows = {
+          preview = true,
+        },
       })
       vim.api.nvim_create_user_command('E', ':lua MiniFiles.open(vim.api.nvim_buf_get_name(0))', { nargs = 0 }) --Edit current path
       vim.api.nvim_create_user_command('EE', ':lua MiniFiles.open(nil, false)', { nargs = 0 })                            --Edit current path
