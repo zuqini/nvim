7884b6fcb9f99fe77d89fedb6de5cffbc29ed194
zuqini <lizuqiliang@gmail.com>
2024-12-06 03:10:09 -0800

    dressing insert_only is default now

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -1,10 +1,6 @@
 return {
   'stevearc/dressing.nvim',
   opts = {
-    input = {
-      -- When true, <Esc> will close the modal - Defaults to true
-      insert_only = false,
-    },
     select = {
       -- Priority list of preferred vim.select implementations
       backend = { "fzf_lua", "telescope", "fzf", "builtin", "nui" },
