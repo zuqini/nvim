174702338b55bbed2a75068bd59bbf9ad745f9b1
zuqini <lizuqiliang@gmail.com>
2024-12-11 01:21:22 -0800

    fix autotag

--- lua/plugins/treesitter/init.lua
+++ lua/plugins/treesitter/init.lua
@@ -1,7 +1,15 @@
 return {
-  { 'windwp/nvim-ts-autotag',     event = 'VeryLazy' },
+  {
+    "windwp/nvim-ts-autotag",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require('nvim-ts-autotag').setup({})
+    end,
+    lazy = true,
+    event = "VeryLazy"
+  },
   'nvim-treesitter/nvim-treesitter-textobjects',
   -- Additional language packs that treesitter doesn't support
-  { 'elubow/cql-vim',             ft = 'cql', },
+  { 'elubow/cql-vim', ft = 'cql', },
   { import = 'plugins/treesitter' }
 }
--- lua/plugins/treesitter/treesitter.lua
+++ lua/plugins/treesitter/treesitter.lua
@@ -20,11 +20,6 @@ return {
 
       ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
       -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-      autotag = {
-        enable = true,
-      },
-
       highlight = {
         -- `false` will disable the whole extension
         enable = true,
