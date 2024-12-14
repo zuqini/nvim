adff1085901d55e4a7108b14929314bcc8480a2e
zuqini <lizuqiliang@gmail.com>
2024-12-14 00:58:57 -0800

    update icons

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -23,12 +23,12 @@ return {
       cmdline = {
         view = "cmdline",
         format = {
-          cmdline = { pattern = "^:", icon = " :", lang = "vim" },
+          cmdline = { pattern = "^:", icon = " :", lang = "vim" },
           search_down = { kind = "search", pattern = "^/", icon = "  /", lang = "regex" },
           search_up = { kind = "search", pattern = "^%?", icon = "  ?", lang = "regex" },
-          filter = { pattern = "^:%s*!", icon = "λ:!", lang = "bash" },
+          filter = { pattern = "^:%s*!", icon = "󰘧 :!", lang = "bash" },
           lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = " :", lang = "lua" },
-          help = { pattern = "^:%s*he?l?p?%s+", icon = " :" },
+          help = { pattern = "^:%s*he?l?p?%s+", icon = " :" },
           inc_rename = { pattern = "^:%s*IncRename%s+", icon = " :" },
           input = { view = "cmdline_input", icon = "󰥻 :" }, -- Used by input()
         },
