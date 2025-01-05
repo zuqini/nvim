af621bdca35d524736af14f5988f71c7625d59ff
zuqini <lizuqiliang@gmail.com>
2025-01-05 15:33:21 -0800

    TrimTrailSpace -> TrimTrail

--- lua/plugins/mini/trailspace.lua
+++ lua/plugins/mini/trailspace.lua
@@ -3,7 +3,7 @@ return {
   version = false,
   config = function()
     require('mini.trailspace').setup()
-      vim.api.nvim_create_user_command('TrimTrailSpace', ':lua MiniTrailspace.trim()', { nargs = 0 })   --Edit current path
+      vim.api.nvim_create_user_command('TrimTrail', ':lua MiniTrailspace.trim()', { nargs = 0 })   --Edit current path
       vim.api.nvim_create_user_command('TrimTrailLine', ':lua MiniTrailspace.trim_last_lines()', { nargs = 0 })   --Edit current path
   end
 };
