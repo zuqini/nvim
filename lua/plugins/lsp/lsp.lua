8389a1161453e00539c31760df40eaf748d3a09c
zuqini <lizuqiliang@gmail.com>
2025-01-13 22:44:19 -0800

    update lazylock

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -58,25 +58,6 @@ return {
       }
     })
 
-    -- local border = {
-    --   { "╭", "FloatBorder" },
-    --   { "─", "FloatBorder" },
-    --   { "╮", "FloatBorder" },
-    --   { "│", "FloatBorder" },
-    --   { "╯", "FloatBorder" },
-    --   { "─", "FloatBorder" },
-    --   { "╰", "FloatBorder" },
-    --   { "│", "FloatBorder" },
-    -- }
-    --
-    -- local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-    -- function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-    --   ---@class vim.lsp.util.open_floating_preview.Opts
-    --   opts = opts or {}
-    --   opts.border = opts.border or border
-    --   return orig_util_open_floating_preview(contents, syntax, opts, ...)
-    -- end
-
     -- refactor to use the new LspAttach autocmd
     local on_attach = function(_, bufnr)
       vim.lsp.inlay_hint.enable(true)
