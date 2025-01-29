f1bf713723afa46bce8ad37f93a3092a31ebe0bd
zuqini <lizuqiliang@gmail.com>
2025-01-28 18:00:45 -0800

    refactor structure

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,12 +1,6 @@
 local vrequire = require("utils").vrequire
 return {
   {
-    "catgoose/nvim-colorizer.lua",
-    event = "BufReadPre",
-    opts = { -- set to setup table
-    },
-  },
-  {
     'mbbill/undotree',
     init = function()
       vrequire('plugins/undotree')
