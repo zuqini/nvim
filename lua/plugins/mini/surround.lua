4141ab0bd690d429e309755b27096070a81c2ab7
zuqini <lizuqiliang@gmail.com>
2025-02-13 00:47:50 -0800

    update flash and surround keymap

--- lua/plugins/mini/surround.lua
+++ lua/plugins/mini/surround.lua
@@ -3,6 +3,25 @@ return {
   version = false,
   event = 'VeryLazy',
   config = function()
-    require('mini.surround').setup()
+    -- emulate vim surround
+    require('mini.surround').setup({
+      mappings = {
+        add = 'ys',
+        delete = 'ds',
+        find = '<leader>sf',
+        find_left = '<leader>sF',
+        highlight = '<leader>sh',
+        replace = 'cs',
+        update_n_lines = '<leader>sn',
+      },
+      search_method = 'cover_or_next',
+    })
+
+    -- Remap adding surrounding to Visual mode selection
+    vim.keymap.del('x', 'ys')
+    vim.keymap.set('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
+
+    -- Make special mapping for "add surrounding for line"
+    vim.keymap.set('n', 'yss', 'ys_', { remap = true })
   end
 }
