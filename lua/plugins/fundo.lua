a7b7e71fd8787227e7b4188f56678978f7a9a233
zuqini <lizuqiliang@gmail.com>
2025-03-06 13:37:20 -0800

    add fundo.lua

---
+++ lua/plugins/fundo.lua
@@ -0,0 +1,12 @@
+return {
+  "kevinhwang91/nvim-fundo",
+  dependencies = {
+    "kevinhwang91/promise-async",
+  },
+  build = function() require("fundo").install() end,
+  lazy = false,
+  config = function()
+    vim.o.undofile = true
+    require('fundo').setup()
+  end,
+}
