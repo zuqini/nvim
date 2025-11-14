cde042d3a1a68a4f9632e8f42d908e04f4259a27
zuqini <lizuqiliang@gmail.com>
2025-11-14 14:02:27 -0800

    diagnostic virtual text personalization

--- lua/options.lua
+++ lua/options.lua
@@ -5,8 +5,15 @@ end
 
 vim.diagnostic.config({
   virtual_text = {
-    prefix = ' ',
-    suffix = '  ',
+    prefix = '󱡞 ',
+    -- prefix = '  ',
+    -- prefix = '  ',
+    -- suffix = '  ',
+    -- suffix = '  ',
+    -- suffix = ' ',
+    -- suffix = '  ',
+    suffix = '  ',
+    -- suffix = ' ',
     virt_text_pos = 'eol_right_align',
   },
   virtual_lines = { current_line = vim.g.virtual_line_current_line },
