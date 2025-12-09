fb50f673dd626b11a0b015e3a0aa9a1f4d06ded6
zuqini <lizuqiliang@gmail.com>
2025-12-08 22:22:50 -0800

    git status in dashboard

--- lua/plugins/snacks/dashboard.lua
+++ lua/plugins/snacks/dashboard.lua
@@ -25,6 +25,20 @@ M.opts = {
       { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
       { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
     },
+    {
+      pane = 2,
+      icon = " ",
+      title = "Git Status",
+      section = "terminal",
+      enabled = function()
+        return Snacks.git.get_root() ~= nil
+      end,
+      cmd = "git status --short --branch --renames",
+      height = 5,
+      padding = 1,
+      ttl = 5 * 60,
+      indent = 3,
+    },
   },
 }
 
