d132298231f37d7080c4db051bb7d311fe76e63b
zuqini <lizuqiliang@gmail.com>
2026-01-08 13:02:41 -0800

    update zpack

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 vim.pack.add({ 'https://github.com/zuqini/zpack.nvim' })
+-- vim.pack.add({ { src = 'https://github.com/zuqini/zpack.nvim', version = '89a5a0ce4542a8433b9b7a94621f58e538ed9f01' } })
 -- vim.pack.add({ { src = vim.fn.expand('~/projects/zpack.nvim'), version = 'main' } })
 
 local vrequire = require("utils").vrequire
