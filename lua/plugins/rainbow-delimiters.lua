5e8335f125fe34dd3b135cb619533694961cbac6
zuqini <lizuqiliang@gmail.com>
2026-03-14 00:37:16 -0700

    workaround for rainbow-delimiters bug

--- lua/plugins/rainbow-delimiters.lua
+++ lua/plugins/rainbow-delimiters.lua
@@ -3,5 +3,13 @@ return {
   cond = not vim.g.vscode,
   event = 'VeryLazy',
   main = 'rainbow-delimiters.setup',
-  opts = {},
+  opts = {
+    -- Neovim 0.11+ returns nil from vim.treesitter.get_parser() instead of
+    -- throwing; rainbow-delimiters doesn't guard against this, crashing on
+    -- scratch buffers (e.g. noice/nui popups) that lack a treesitter parser.
+    condition = function(bufnr)
+      local ok, parser = pcall(vim.treesitter.get_parser, bufnr)
+      return ok and parser ~= nil
+    end,
+  },
 }
