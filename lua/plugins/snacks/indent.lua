62362bb6f946b3acd2ea53ffc83cd1a273f8e66f
zuqini <lizuqiliang@gmail.com>
2026-04-02 17:27:34 -0700

    fix fragile snacks indent highlight setting, by reapplying them on colorscheme change

--- lua/plugins/snacks/indent.lua
+++ lua/plugins/snacks/indent.lua
@@ -1,12 +1,17 @@
 local M = {}
 
-vim.api.nvim_set_hl(0, "SnacksIndent1", { fg = "#662121" })
-vim.api.nvim_set_hl(0, "SnacksIndent2", { fg = "#767621" })
-vim.api.nvim_set_hl(0, "SnacksIndent3", { fg = "#216631" })
-vim.api.nvim_set_hl(0, "SnacksIndent4", { fg = "#325a5e" })
-vim.api.nvim_set_hl(0, "SnacksIndent5", { fg = "#324b7b" })
-vim.api.nvim_set_hl(0, "SnacksIndent6", { fg = "#562155" })
-vim.api.nvim_set_hl(0, "SnacksIndentScope", { fg = "#00FF00" })
+local function set_indent_hl()
+  vim.api.nvim_set_hl(0, "SnacksIndent1", { fg = "#662121" })
+  vim.api.nvim_set_hl(0, "SnacksIndent2", { fg = "#767621" })
+  vim.api.nvim_set_hl(0, "SnacksIndent3", { fg = "#216631" })
+  vim.api.nvim_set_hl(0, "SnacksIndent4", { fg = "#325a5e" })
+  vim.api.nvim_set_hl(0, "SnacksIndent5", { fg = "#324b7b" })
+  vim.api.nvim_set_hl(0, "SnacksIndent6", { fg = "#562155" })
+  vim.api.nvim_set_hl(0, "SnacksIndentScope", { fg = "#00FF00" })
+end
+
+set_indent_hl()
+vim.api.nvim_create_autocmd("ColorScheme", { callback = set_indent_hl })
 
 M.opts = {
   indent = {
